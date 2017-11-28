7888467b5f1d0ca8c1103b24b52068da09372299
RGHuynh <randall.g.huynh@gmail.com>
2017-11-28 13:26:36 -0500

    Add another exit programm after two blanks

---
+++ downloads/drive-download-20171128T054403Z-001/cheering-mascot-challenge-master/mascot.rb
@@ -0,0 +1,45 @@
+# Get input from the user:  the cheer
+def call_out_cheer
+    p "write some shit!"
+    count = 1
+    user_cheer = gets.chomp
+    until user_cheer != "" || count == 2
+      count += 1
+      user_cheer = gets.chomp
+    end
+    user_cheer
+end
+
+# Determine the mascot's response based on the argument
+# passed to the method
+def mascot_sign_for(input)
+    case input
+    when "RED HOT"
+        return 'H-O-T!'
+    when "DO IT AGAIN"
+        return "Go, Fight, Win"
+    when "2 BITS"
+        return "Holler!"
+    when "STOMP YOUR FEET"
+        return "STOMP!"
+    else
+        return "Go Team!"
+    end
+end
+
+# Print the argument passed to the method
+def display(response)
+    p response
+end
+
+# This method will control the flow of the application,
+# making use of the other three methods.
+def coordinate_cheers
+    user_cheer = call_out_cheer
+    
+    until user_cheer == "GAME OVER" || user_cheer == ""
+      mascot_sign = mascot_sign_for(user_cheer)
+      display(mascot_sign)
+      user_cheer = call_out_cheer
+    end
+end
---
+++ downloads/drive-download-20171128T054403Z-001/cheering-mascot-challenge-master/runner.rb
@@ -0,0 +1,6 @@
+require_relative "mascot"
+
+p 'cheer!'
+# This line calls the coordinate_cheers method,
+# starting the cheers.
+coordinate_cheers
